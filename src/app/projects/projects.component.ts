75398a3aff39679dafea905e4325538547025e9c
ptk244 <94042348+ptk244@users.noreply.github.com>
2025-07-29 14:52:34 +0530

    tre

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -11,7 +11,32 @@ export class ProjectsComponent implements OnInit {
 
   projects: any[] = [];
 
+  
+
   ngOnInit(): void {
-    this.projects = JSON.parse(localStorage.getItem('projects') || '[]');
+    // this.projects = JSON.parse(localStorage.getItem('projects') || '[]');
+
+    this.projects = [
+      {
+        title: 'Codial - Social Media Web Application',
+        description: 'A real-time social media platform with chat functionality, built with Node.js, Express.js, and MongoDB.',
+        link: 'https://github.com/ptk244/codial',
+        image: 'assets/codial.jpg' // Replace with your image path
+      },
+      {
+        title: 'Food Mine',
+        description: 'An e-cart website allowing users to order food from various restaurants, using Angular and Node.js.',
+        link: 'https://github.com/ptk244/foodmine',
+        image: 'assets/Foodmine.png'
+      },
+      {
+        title: 'Jio Coupons',
+        description: 'Enhanced user interface and experience of Jio Coupons website using Angular and Django.',
+        link: 'https://jiocoupons.in/',
+        image: 'assets/Jiocoupons.jpg'
+      }
+    ];
   }
+
+  
 }
